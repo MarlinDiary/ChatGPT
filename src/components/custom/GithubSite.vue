9a7526f7b310a270415a6445a3cf4e1df9e3bfd7
Marlin <50492264+MarlinDiary@users.noreply.github.com>
2023-03-02 13:58:13 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <span>View on</span>
+    <a href="https://marlin.red" target="_blank" class="text-blue-500">
+      Marlin
     </a>
   </div>
 </template>
